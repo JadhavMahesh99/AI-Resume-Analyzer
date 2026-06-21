6788eb42e464f70bba9af8c30d0f664decf7744c
JadhavMahesh99 <163672680+JadhavMahesh99@users.noreply.github.com>
2026-06-21 15:50:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+skills = [
+    "Python",
+    "Java",
+    "SQL",
+    "Git",
+    "Machine Learning",
+    "Data Structures"
+]
+
+resume_text = """
+Mahesh Jadhav
+Skills: Python, Java, Machine Learning
+"""
+
+found_skills = []
+
+for skill in skills:
+    if skill.lower() in resume_text.lower():
+        found_skills.append(skill)
+
+print("Skills Found:")
+
+for skill in found_skills:
+    print("-", skill)
+
+score = len(found_skills) * 15
+
+print("\nResume Score:", score)
